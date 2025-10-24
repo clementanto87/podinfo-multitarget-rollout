2b7137a074e9aac8cc6b5afdf33eabba3d8579e5
clement.anto <clement.anto@outfittery.de>
2025-10-24 15:43:03 +0200

    modules

--- infra/ec2/variables.tf
+++ infra/ec2/variables.tf
@@ -26,19 +26,16 @@ variable "instance_type" {
 variable "desired_capacity" {
   description = "Desired number of instances in ASG"
   type        = number
-  default = 1
 }
 
 variable "min_size" {
   description = "Minimum number of instances in ASG"
   type        = number
-  default = 1
 }
 
 variable "max_size" {
   description = "Maximum number of instances in ASG"
   type        = number
-  default = 2
 }
 
 variable "enable_scaling" {
