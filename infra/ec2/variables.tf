157487abdb57e28977e9a57dad75eaf2dcf93a51
clement.anto <clement.anto@outfittery.de>
2025-10-25 13:09:09 +0200

    fix the refactoring issue

--- infra/ec2/variables.tf
+++ infra/ec2/variables.tf
@@ -1,6 +1,9 @@
+# infra/ec2/variables.tf
+
 variable "region" {
   description = "AWS region"
   type        = string
+  default     = "eu-west-1"
 }
 
 variable "deploy_env" {
@@ -11,36 +14,43 @@ variable "deploy_env" {
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "List of public subnet CIDR blocks"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
+  default     = "t3.micro"
 }
 
 variable "desired_capacity" {
   description = "Desired number of instances in ASG"
   type        = number
+  default     = 1
 }
 
 variable "min_size" {
   description = "Minimum number of instances in ASG"
   type        = number
+  default     = 1
 }
 
 variable "max_size" {
   description = "Maximum number of instances in ASG"
   type        = number
+  default     = 2
 }
 
 variable "enable_scaling" {
   description = "Enable auto scaling"
   type        = bool
+  default     = false
 }
 
 variable "ecr_repo_url" {
@@ -49,11 +59,11 @@ variable "ecr_repo_url" {
 }
 
 variable "image_digest" {
-  description = "Docker image digest"
+  description = "Docker image digest (e.g., sha256:...)"
   type        = string
 }
 
 variable "super_secret_token_arn" {
-  description = "ARN of the secret in Secrets Manager"
+  description = "ARN of the secret in AWS Secrets Manager"
   type        = string
 }
